b056d1e6b49fbceca64221203184f5a399aaa9da
Brandon Carlos <brandon_marzo@hotmail.com>
2024-07-23 14:48:42 -0300

    Implementing service right now

--- src/main/java/com/example/Mail/controller/CovidController.java
+++ src/main/java/com/example/Mail/controller/CovidController.java
@@ -1,9 +1,23 @@
 package com.example.Mail.controller;
 
+import com.example.Mail.service.CovidService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/covid")
+@RequiredArgsConstructor //Constructor in format of annotation
 public class CovidController {
+
+    private final CovidService covidService;
+
+    @GetMapping("/get-all-country-covid-data")
+    public ResponseEntity<?> callRapidEndpointToGetCovidData() {
+        return ResponseEntity.ok(covidService.getAllCountryCovidData());
+    }
+
+
 }
---
+++ src/main/java/com/example/Mail/service/CovidService.java
@@ -0,0 +1,12 @@
+package com.example.Mail.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CovidService {
+
+    private static final url = "";
+
+    public Object getAllCountryCovidData() {
+    }
+}
